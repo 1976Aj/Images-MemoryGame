67778e75989fa1ce927bb97b9654a8521a79006d
Anna Jacknitsky <annajacknitsky@Annas-MacBook-Pro.local>
2017-07-30 15:16:23 -0600

    I created a memory game to practice JavaScript. I used the class images

---
+++ scripts/min/main-min.js
@@ -0,0 +1,124 @@
+// $( document ).ready(function() {
+  var BoxOpened = "";
+  var ImgOpened = "";
+  var Counter = 0;
+  var ImgFound = 0;
+
+  var Source = "#boxcard";
+
+  var ImgSource = [
+  "/images/aj.jpg",
+  "/images/eddie.png",
+  "/images/et.png",
+  "/images/grace.png",
+  "/images/jesse.png",
+  "/images/jh.png",
+  "/images/js.png",
+  "/images/margaret.png",
+  "/images/matt.png",
+  "/images/mr.png",
+  "/images/sergio.png",
+  "/images/stephen.png"
+];
+
+//Creates a random function that takes in two arguments "MaxValue" and "MinValue"
+//This returns a random number between the specified values. The returned value is no lower than (and may possibly equal) min, and is less than (but not equal to) max.
+function RandomFunction(MaxValue, MinValue) {
+		return Math.round(Math.random() * (MaxValue - MinValue) + MinValue);
+	}
+
+//ImgAll returns all the children of the elements in the array, and the first child is assigned an index of 0
+//ImgThis concatenates the Source with the first child(element) in the div
+function ShuffleImages() {
+	var ImgAll = $(Source).children();
+	var ImgThis = $(Source + " div:first-child");
+	var ImgArr = new Array();
+//ImgThis.next() pulls the next image in the array
+	for (var i = 0; i < ImgAll.length; i++) {
+		ImgArr[i] = $("#" + ImgThis.attr("id") + " img").attr("src");
+		ImgThis = ImgThis.next();
+	}
+		ImgThis = $(Source + " div:first-child");
+
+	for (var z = 0; z < ImgAll.length; z++) {
+	var RandomNumber = RandomFunction(0, ImgArr.length - 1);
+
+//ImgArr.splice(RandomNumber, 1) sets at which point to start changing the array, and how many to remove
+		$("#" + ImgThis.attr("id") + " img").attr("src", ImgArr[RandomNumber]);
+		ImgArr.splice(RandomNumber, 1);
+		ImgThis = ImgThis.next();
+	}
+}
+
+function ResetGame() {
+	ShuffleImages();
+	$(Source + " div img").hide();
+	$(Source + " div").css("visibility", "visible");
+	Counter = 0;
+	$("#success").remove();
+	$("#counter").html("" + Counter);
+	BoxOpened = "";
+	ImgOpened = "";
+	ImgFound = 0;
+	return false;
+}
+
+//The .unbind removes a previusly-attached event handler from the element
+function OpenCard() {
+	var id = $(this).attr("id");
+
+	if ($("#" + id + " img").is(":hidden")) {
+		$(Source + " div").unbind("click", OpenCard);
+
+		$("#" + id + " img").slideDown('fast');
+
+//The .bind attaches a handler to an event for the element
+		if (ImgOpened == "") {
+			BoxOpened = id;
+			ImgOpened = $("#" + id + " img").attr("src");
+			setTimeout(function() {
+				$(Source + " div").bind("click", OpenCard)
+			}, 300);
+		} else {
+			CurrentOpened = $("#" + id + " img").attr("src");
+			if (ImgOpened != CurrentOpened) {
+				setTimeout(function() {
+					$("#" + id + " img").slideUp('fast');
+					$("#" + BoxOpened + " img").slideUp('fast');
+					BoxOpened = "";
+					ImgOpened = "";
+				}, 400);
+			} else {
+				$("#" + id + " img").parent().css("visibility", "hidden");
+				$("#" + BoxOpened + " img").parent().css("visibility", "hidden");
+				ImgFound++;
+				BoxOpened = "";
+				ImgOpened = "";
+			}
+			setTimeout(function() {
+				$(Source + " div").bind("click", OpenCard)
+			}, 400);
+		}
+		Counter++;
+		$("#counter").html("" + Counter);
+
+		if (ImgFound == ImgSource.length) {
+			$("#counter").prepend('<span id="success">You Found All Pictures With </span>');
+		}
+	}
+}
+
+$(function() {
+
+for (var y = 1; y < 3 ; y++) {
+	$.each(ImgSource, function(i, val) {
+		$(Source).append("<div id=card" + y + i + "><img src=" + val + " />");
+	});
+}
+	$(Source + " div").click(OpenCard);
+	ShuffleImages();
+});
+
+// });
+
+
